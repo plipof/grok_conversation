ca54fdc6c03a2a02388886c2ff6269cc95ab101a
Brayton <brayton.stafford@gmail.com>
2025-01-26 18:51:32 -0600

    Fixed file name

--- custom_components/grok_conversation/config_flow.py
+++ custom_components/grok_conversation/config_flow.py
@@ -93,7 +93,7 @@ class OpenAIConfigFlow(ConfigFlow, domain=DOMAIN):
             errors["base"] = "unknown"
         else:
             return self.async_create_entry(
-                title="ChatGPT",
+                title="Grok",
                 data=user_input,
                 options=RECOMMENDED_OPTIONS,
             )
