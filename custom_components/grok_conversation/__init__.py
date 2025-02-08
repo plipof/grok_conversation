22d4a9860225a100f3a38b8bc92f715dabccaa2d
Brayton <brayton.stafford@gmail.com>
2025-02-07 18:28:17 -0600

    Bug fix

--- custom_components/grok_conversation/__init__.py
+++ custom_components/grok_conversation/__init__.py
@@ -89,10 +89,10 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: OpenAIConfigEntry) -> bool:
     """Set up Grok Conversation from a config entry."""
+    openai.base_url = "https://api.x.ai/v1"
     client = openai.AsyncOpenAI(
         api_key=entry.data[CONF_API_KEY],
-        http_client=get_async_client(hass),
-        base_url="https://api.x.ai/v1",
+        http_client=get_async_client(hass)
     )
 
     # Cache current platform data which gets added to each request (caching done by library)
